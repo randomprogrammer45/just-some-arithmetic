be55e316366e2090cec35295935d32bcb3c51622
randomprogrammer45 <95759928+randomprogrammer45@users.noreply.github.com>
2021-12-12 15:39:21 +0100

    yes
    the main thing

---
+++ arithmetic.js
@@ -0,0 +1,12 @@
+function add(num, numa) {
+return num + numa;
+}
+function timesAnything(num, y) {
+    return num * y;
+    }
+function subtract(num, y) {
+return num - y;
+}
+function divide(num, y) {
+return num / y;
+}
